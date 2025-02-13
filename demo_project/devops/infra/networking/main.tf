ba3a380aa14e81c6f906faa219fcf8f291271d2c
mashru020 <mashrurrahman16@gmail.com>
2025-02-13 18:26:40 +0600

    network setup

--- demo_project/devops/infra/networking/main.tf
+++ demo_project/devops/infra/networking/main.tf
@@ -32,3 +32,27 @@ resource "aws_subnet" "dev_proj_1_private_subnet" {
     Name = "dev-proj-1-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
+  vpc_id = aws_vpc.dev_proj_1_vpc.id
+  tags = {
+    Name = "dev-proj-1-igw"
+  }
+}
+resource "aws_route_table" "dev_proj_1_public_route_table" {
+  vpc_id = aws_vpc.dev_proj_1_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_proj_1_public_internet_gateway.id
+  }
+  tags = {
+    Name = "dev-proj-1-public-rt"
+  }
+}
+
+resource "aws_route_table" "dev_porj_1_private_route_table" {
+  vpc_id = aws_vpc.dev_proj_1_vpc.id
+  tags = {
+    Name = "dev-proj-1-private-rt"
+  }
+}
